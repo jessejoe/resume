ffa56e00feda1f69e68ffc555ddd076178f44970
jessejoe <jesse@jessejoe.com>
2016-06-15 02:20:08 -0400

    Add my resume, add theme for separate PDF and HTML generation (#2)

--- build.py
+++ build.py
@@ -96,12 +96,12 @@ def make_html(config, data):
 
 def make_pdf(config, data):
     """
-    Generate PDF file out of generated 'index.html' page.
+    Generate PDF file out of generated 'pdf.html' page.
     """
     from weasyprint import HTML
     output_dir = config.get('output_dir', 'build')
     output_file = os.path.join(output_dir, config.get('pdf_file', 'resume.pdf'))
-    input_file = os.path.join(output_dir, 'index.html')
+    input_file = os.path.join(output_dir, 'pdf.html')
     theme_location = os.path.join('themes', config['theme'])
     html = HTML(input_file, base_url=theme_location)
     html.write_pdf(output_file)
